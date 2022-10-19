474997b5624f5b14289380c7617b243d53a2e658
Ekstromen <113119290+Ekstromen@users.noreply.github.com>
2022-10-19 20:07:03 +0300

    Add files via upload

---
+++ lab10/lan10(1).c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+double fa(double x);
+double fb(double x);
+
+void main()
+{
+	double x;
+	setlocale(LC_ALL, "rus");
+	printf("Введите x\n");
+	scanf("%lf", &x);
+	printf("произведение функций = %lf; разность квадратов функций = %lf; удвоенная сумма функций = %lf", fa(x) * fb(x), pow(fa(x), 2) - pow(fb(x), 2), 2 * (fa(x) + fb(x)));
+
+}
+
+double fa(double x)
+{
+	if (x <= 3) {
+		return (pow(x, 2) - (3 * x) + 9);
+	}
+	else {
+		return (1 / (pow(x, 3) + 3));
+	}
+}
+
+double fb(double x)
+{
+	return (x * exp(sin(pow(x, 2))));
+}
