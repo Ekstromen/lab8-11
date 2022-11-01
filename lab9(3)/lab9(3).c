bd77aef8b76edc5b9c5e39fede9362c959120df1
Ekstromen <113119290+Ekstromen@users.noreply.github.com>
2022-11-01 16:34:30 +0300

    Add files via upload

---
+++ lab9(3)/lab9(3).c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int figure(int x, int y, int r);
+
+void main()
+{
+	int x, y, X, Y,b,a,r;
+	setlocale(LC_ALL, "rus");
+	printf("¬ведите радиус: \n");
+	scanf("%d", &r);
+	printf("¬ведите малую сторону: \n");
+	scanf("%d", &a);
+
+	X = r * 2;
+	Y = r * 2;
+ 	b = sqrt(pow(2*r,2) - pow(a,2));
+
+	for (y = -(Y / 2); y <= Y / 2; ++y)
+	{
+		for (x = -(X / 2); x <= X / 2; ++x)
+		{
+			if (figure(x, y, r) > figure(x,y,r-2)) printf("x ");
+			else printf("  ");
+		}
+		printf("\n");
+	}
+
+
+}
+
+int figure(int x, int y, int r)
+{
+	if ((y > ((x - r) / 2)) && (y < -((x - r) / 2)) && (y < x + r) && (y > -(x + r))) return 1;
+	else return 0;
+
+
+}	
